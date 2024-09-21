9333d9c346e16dc65c59a2446e3698381943b337
cms66 <58144229+cms66@users.noreply.github.com>
2024-09-21 17:49:57 +0100

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -5,3 +5,4 @@ curl -L https://raw.githubusercontent.com/gitbls/sdm/master/EZsdmInstaller | bas
 wget https://downloads.raspberrypi.org//raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
 wget https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64.img.xz
 unxz *.xz
+
