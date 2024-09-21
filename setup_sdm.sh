71e16650cd61789e0064e981ba3c1d43f5adaed9
cms66 <58144229+cms66@users.noreply.github.com>
2024-09-21 17:16:15 +0100

    Create setup_sdm.sh

---
+++ setup_sdm.sh
@@ -0,0 +1,7 @@
+# Install SDM
+curl -L https://raw.githubusercontent.com/gitbls/sdm/master/EZsdmInstaller | bash
+
+# Download latest images and extract
+wget https://downloads.raspberrypi.org//raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
+wget https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64.img.xz
+unxz *.xz
