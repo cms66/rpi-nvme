5445b091625a9d038067af4dd0f6042148c563fe
cms66 <58144229+cms66@users.noreply.github.com>
2024-09-21 18:00:10 +0100

    Update setup_base.sh

--- setup_base.sh
+++ setup_base.sh
@@ -3,5 +3,9 @@
 # - rpi imager used to configure user/hostname
 # sudo run this script as created user
 
+# Install/update software
+apt-get -y update
+apt-get -y upgrade
+# apt-get -y install
 # Update firmware
 rpi-eeprom-update -a
