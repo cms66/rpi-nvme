ad974fcc52efe53642667cc18926f7c2e1593694
cms66 <58144229+cms66@users.noreply.github.com>
2024-09-23 13:20:56 +0100

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -1,10 +1,12 @@
 # Install SDM
-apt-get update
-apt-get upgrade
 curl -L https://raw.githubusercontent.com/gitbls/sdm/master/EZsdmInstaller | bash
 
 # Download latest images and extract
-wget https://downloads.raspberrypi.org//raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
-wget https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64.img.xz
+read -p "Path to directory for images (press enter for default = $usrpath/share$pinum/sdm/): " userdir
+imgdir=${userdir:="$usrpath/share$pinum/sdm/"}
+mkdir $imgdir
+wget -P $imgdir https://downloads.raspberrypi.org//raspios_lite_arm64/images/raspios_lite_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64-lite.img.xz
+wget -P $imgdir https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2024-07-04/2024-07-04-raspios-bookworm-arm64.img.xz
+wget -P $imgdir https://downloads.raspberrypi.com/raspios_lite_armhf/images/raspios_lite_armhf-2024-07-04/2024-07-04-raspios-bookworm-armhf-lite.img.xz
 unxz *.xz
 
