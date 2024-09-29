cbcde29d9f821a03d804c5440935d23994e39ed2
cms66 <58144229+cms66@users.noreply.github.com>
2024-09-29 19:46:02 +0100

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -2,6 +2,8 @@
 # TODO
 # - Add option for install location (default = /usr/local)
 # - Add option for setting image directory (defaults to local share for performance)
+# - Add option for version change e.g Bullseye/Bookworm
+# Add check for latest update for current and last versions
 
 imgdir=$usrpath/share$pinum/sdm/images
 # Latest images
@@ -27,6 +29,7 @@ install_local()
  	mkdir -p $usrpath/share$pinum/sdm/images/current
   	mkdir -p $usrpath/share$pinum/sdm/images/latest
    	mkdi -p $usrpath/share$pinum/sdm/images/archive
+    	chmod -R $usrname:$usrname $usrpath/share$pinum/sdm
  	download_latest_images
 }
 
@@ -36,7 +39,7 @@ install_server()
  	if grep -F "/usr/local" "/etc/exports"; then
   		echo "export exists"
   	else
-		  echo "/usr/local $localnet(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+		echo "/usr/local $localnet(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
   		exportfs -ra
       #ufw allow from $localnet to $localnet
 	fi
@@ -45,13 +48,22 @@ install_server()
 
 download_latest_images()
 {
+	# Replace uncustomized latest images
+  	rm $imgdir/latest/*.img
 	# Download latest images and extract
-	wget -P $imgdir $url64lite
- 	wget -P $imgdir $url64desk
-  	wget -P $imgdir $url32lite
-   	wget -P $imgdir $url32desk
-    	unxz $imgdir/*.xz
-	read -p "Downloads to $imgdir complete, press enter to return to menu" input
+	wget -P $imgdir/latest $url64lite
+ 	wget -P $imgdir/latest $url64desk
+  	wget -P $imgdir/latest $url32lite
+   	wget -P $imgdir/latest $url32desk
+    	unxz $imgdir/latest/*.xz
+     	read -p "Downloads to $imgdir/latest complete, press enter to return to menu" input
+}
+
+customize_image()
+{
+	
+	read -p "Password for $usrname: " usrpass
+	sdm --customize --plugin user:"adduser=$usrname|password=$usrpass" --plugin L10n:host --plugin disables:piwiz --expand-root --regen-ssh-host-keys --restart 
 }
 
 show_sdm_menu
