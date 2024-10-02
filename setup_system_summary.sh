cb720d1f5b16b48ea3499ba16d04a8482882f606
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-02 20:13:01 +0100

    Update setup_system_summary.sh

--- setup_system_summary.sh
+++ setup_system_summary.sh
@@ -4,8 +4,8 @@ printf "Model: $pimodel \n"
 printf "Revision: $pirev \n"
 printf "Architecture: $osarch \n"
 printf "Firmware: $(rpi-eeprom-update) \n"
-printf "\nMemory: \n$pimem \n"
-printf "\nStorage: $(lsblk) \n"
+printf "\nMemory:\n$pimem \n"
+printf "\nStorage:\n$(lsblk) \n"
 printf "Firewall "
 ufw status
 read -p "Press enter to return to menu" input
