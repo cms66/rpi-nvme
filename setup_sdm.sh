02b53f3b980878c5197f9d32069976cb20adca6e
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-06 21:50:46 +0100

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -67,6 +67,7 @@ read_config()
 		#arrconf[$key]="$value"
 		arrconf[$key]="$value"
 	done < $instdir/custom.conf
+ 	echo ${arrconf[wifissid]}
  	read -p "Current config, press enter to return to menu" input
 }
 
