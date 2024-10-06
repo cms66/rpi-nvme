df9dd033029684deca056ac88864af1f009b7702
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-06 10:57:18 +0100

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -37,15 +37,15 @@ read_config()
     	#	echo "$value"
 	#done 
 	#conf=$instdir/custom.conf
- 	#conf=$(grep -v ^\# $instdir/custom.conf)
+ 	conf=$(grep -v ^\# $instdir/custom.conf)
 	while IFS= read -r line
 	do
     		key=${line%% *}
     		value=${line#* }
     		echo "Key: [$key]"
     		echo "Value: [$value]"
-	#done < "$conf"
- 	done < $(grep -v ^\# $instdir/custom.conf)
+	done < "$conf"
+ 	#done < $(grep -v ^\# $instdir/custom.conf)
   	read -p "Current config, press enter to return to menu" input
 }
 
