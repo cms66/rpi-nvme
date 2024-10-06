919ed92b0289dde60b57afeb5477004150ed5ab4
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-06 13:04:28 +0100

    Update setup_sdm.sh

--- setup_sdm.sh
+++ setup_sdm.sh
@@ -12,8 +12,8 @@ function handle_error
 {
     #echo "An error occurred: $1"
     # Additional error handling code can go here
-    #echo "$(caller): ${BASH_COMMAND}\n"
-    read -p "$(caller): ${BASH_COMMAND}\n" inp
+    echo "$(caller): ${BASH_COMMAND}"
+    #read -p "$(caller): ${BASH_COMMAND}\n" inp
 }
 
 # Set the error handler to be called when an error occurs
