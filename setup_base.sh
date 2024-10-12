06e4718957ef28bbacd9b23e80845268ec68fead
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-12 15:32:23 +0100

    Update setup_base.sh

--- setup_base.sh
+++ setup_base.sh
@@ -57,7 +57,8 @@ pimodelnum=$(cat /sys/firmware/devicetree/base/model | cut -d " " -f 3)
 
 # Configure firewall (ufw)
 # Allow SSH from local subnet only, unless remote access needed
-read -p "Allow remote (outside home network) ssh access (y/n): " input
+#read -p "Allow remote (outside home network) ssh access (y/n): " input # TODO
+input="n"
 if [ X$input = X"n" ]
 then
 	ufw allow from $localnet to any port ssh
