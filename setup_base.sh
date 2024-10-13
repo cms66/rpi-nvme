122d124f2b065abeabfbb223b9f71765b3723fa6
cms66 <58144229+cms66@users.noreply.github.com>
2024-10-13 16:18:55 +0100

    Update setup_base.sh

--- setup_base.sh
+++ setup_base.sh
@@ -6,12 +6,12 @@
 set -e
 
 # Set default shell to bash
-dpkg-divert --remove --no-rename /usr/share/man/man1/sh.1.gz
-dpkg-divert --remove --no-rename /bin/sh
-ln -sf bash.1.gz /usr/share/man/man1/sh.1.gz
-ln -sf bash /bin/sh
-dpkg-divert --add --local --no-rename /usr/share/man/man1/sh.1.gz
-dpkg-divert --add --local --no-rename /bin/sh
+#dpkg-divert --remove --no-rename /usr/share/man/man1/sh.1.gz
+#dpkg-divert --remove --no-rename /bin/sh
+#ln -sf bash.1.gz /usr/share/man/man1/sh.1.gz
+#ln -sf bash /bin/sh
+#dpkg-divert --add --local --no-rename /usr/share/man/man1/sh.1.gz
+#dpkg-divert --add --local --no-rename /bin/sh
 
 # Error handler
 handle_error()
@@ -29,38 +29,38 @@ repo="rpi-nvme"
 pimodelnum=$(cat /sys/firmware/devicetree/base/model | cut -d " " -f 3)
 
 # Install/update software
-apt-get -y update
-apt-get -y upgrade
-apt-get -y install python3-dev gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress zip nfs-common fail2ban ufw ntpdate
+#apt-get -y update
+#apt-get -y upgrade
+#apt-get -y install python3-dev gcc g++ gfortran libraspberrypi-dev libomp-dev git-core build-essential cmake pkg-config make screen htop stress zip nfs-common fail2ban ufw ntpdate
 
 # Git setup
-mkdir /home/$usrname/.pisetup
-cd /home/$usrname/.pisetup
-git clone https://github.com/cms66/$repo.git
-chown -R $usrname:$usrname /home/$usrname/.pisetup
+#mkdir /home/$usrname/.pisetup
+#cd /home/$usrname/.pisetup
+#git clone https://github.com/cms66/$repo.git
+#chown -R $usrname:$usrname /home/$usrname/.pisetup
 
 # Add bash alias for setup and test menu
-echo "alias mysetup=\"sudo bash ~/.pisetup/$repo/setup_menu.sh\"" >> /home/$usrname/.bashrc
-echo "alias mytest=\"sudo bash ~/.pisetup/$repo/test_menu.sh\"" >> /home/$usrname/.bashrc
+#echo "alias mysetup=\"sudo bash ~/.pisetup/$repo/setup_menu.sh\"" >> /home/$usrname/.bashrc
+#echo "alias mytest=\"sudo bash ~/.pisetup/$repo/test_menu.sh\"" >> /home/$usrname/.bashrc
 
 # - Create python Virtual Environment (with access to system level packages) and bash alias for activation
-python -m venv --system-site-packages /home/$usrname/.venv
-echo "alias myvp=\"source ~/.venv/bin/activate\"" >> /home/$usrname/.bashrc
-echo "alias dvp=\"deactivate\"" >> /home/$usrname/.bashrc
-chown -R $usrname:$usrname /home/$usrname/.venv
+#python -m venv --system-site-packages /home/$usrname/.venv
+#echo "alias myvp=\"source ~/.venv/bin/activate\"" >> /home/$usrname/.bashrc
+#echo "alias dvp=\"deactivate\"" >> /home/$usrname/.bashrc
+#chown -R $usrname:$usrname /home/$usrname/.venv
 
 # create local folder structure for created user with code examples
-tar -xvzf /home/$usrname/.pisetup/$repo/local.tgz -C /home/$usrname
-chown -R $usrname:$usrname /home/$usrname/local/
+#tar -xvzf /home/$usrname/.pisetup/$repo/local.tgz -C /home/$usrname
+#chown -R $usrname:$usrname /home/$usrname/local/
 
 # Configure fail2ban
-cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
-cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+#cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+#cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
 # Configure firewall (ufw)
 # Allow SSH from local subnet only, unless remote access needed
 read -rp "Allow remote acces (y/n): " inp
-if [[ X$inp = X"n" ]]
+if [ X$inp = X"n" ]
 then
  	yes | sudo ufw allow from $localnet to any port ssh
 else
